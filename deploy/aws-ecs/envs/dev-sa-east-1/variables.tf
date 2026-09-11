b810042b036d4eece41e19c79e94acd2121b3122
Leonardo Silva <lsilva@austa.com.br>
2026-09-11 10:32:30 -0300

    chore(canal-teste): aponta para a imagem 048e234e (pagina corrigida)
    O default descreve o que roda. Sem este bump o canal continuaria servindo a pagina que
    anuncia "nao escalou" quando o numero foi reaproveitado.

--- deploy/aws-ecs/envs/dev-sa-east-1/variables.tf
+++ deploy/aws-ecs/envs/dev-sa-east-1/variables.tf
@@ -422,7 +422,7 @@ variable "canal_teste_image_tag" {
     de funcionar.
   EOT
   type        = string
-  default     = "a0fbb729" # rota /receptor/simular + escalonamento.html (11/09/2026)
+  default     = "048e234e" # pagina corrigida: nao anuncia mais "nao escalou" em numero reaproveitado (11/09/2026)
 }
 
 variable "canal_teste_desired_count" {
